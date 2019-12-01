be089ca5616442e6719ca656396e62186ec161d9
Estefan Hu <estefanhu074@gmail.com>
2019-12-01 09:53:55 -0800

    Installed Greeting

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(bodyParser.json({ limit: '500kb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '500kb' }));
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 app.use(multer().none());
 
 const uri = process.env.ATLAS_URI;
@@ -21,6 +22,10 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
   });
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html');
+})
 
 const stagesRouter = require('./routes/stages');
 app.use('/stages', stagesRouter);
